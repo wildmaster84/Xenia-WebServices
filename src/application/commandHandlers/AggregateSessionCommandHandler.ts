683a769d6381778fba6567d836b59c3fabb4a5c5
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2024-01-11 01:05:19 +0000

    Improved home page

--- src/application/commandHandlers/AggregateSessionCommandHandler.ts
+++ src/application/commandHandlers/AggregateSessionCommandHandler.ts
@@ -4,6 +4,10 @@ import { AggregateSessionCommand } from '../commands/AggregateSessionCommand';
 import ISessionRepository, {
   ISessionRepositorySymbol,
 } from 'src/domain/repositories/ISessionRepository';
+import Session from 'src/domain/aggregates/Session';
+import { readFile } from 'fs/promises';
+
+const titleIdToTitleMap = {};
 
 @CommandHandler(AggregateSessionCommand)
 export class AggregateSessionCommandHandler
@@ -12,9 +16,36 @@ export class AggregateSessionCommandHandler
   constructor(
     @Inject(ISessionRepositorySymbol)
     private repository: ISessionRepository,
-  ) {}
+  ) {
+    this.LoadTitleIds();
+  }
+
+  async LoadTitleIds() {
+    const title_ids_file = await readFile('title_ids.json');
+    const ids = JSON.parse(title_ids_file.toString('utf8'));
+
+    ids.forEach((title) => {
+      titleIdToTitleMap[title.titleid] = title.title;
+    });
+  }
 
   async execute() {
-    return this.repository.findAllAdvertisedSessions();
+    const sessions = await this.repository.findAllAdvertisedSessions();
+
+    const titles = {};
+
+    sessions.forEach(async (session: Session) => {
+      const title =
+        titleIdToTitleMap[session.titleId.toString()] + ' - ' + session.titleId;
+      const players = session.players.length;
+
+      if (!titles[title]) {
+        titles[title] = [];
+      }
+
+      titles[title].push({ players: players });
+    });
+
+    return JSON.stringify(titles);
   }
 }
--- src/infrastructure/presentation/controllers/index.controller.ts
+++ src/infrastructure/presentation/controllers/index.controller.ts
@@ -3,9 +3,6 @@ import ILogger, { ILoggerSymbol } from '../../../ILogger';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { readFile } from 'fs/promises';
 import { AggregateSessionCommand } from 'src/application/commands/AggregateSessionCommand';
-import Session from 'src/domain/aggregates/Session';
-
-const titleIdToTitleMap = {};
 
 @Controller()
 export class IndexController {
@@ -13,41 +10,22 @@ export class IndexController {
     @Inject(ILoggerSymbol) private readonly logger: ILogger,
     private readonly queryBus: QueryBus,
     private readonly commandBus: CommandBus,
-  ) {
-    this.LoadTitleIds();
-  }
-
-  async LoadTitleIds() {
-    const title_ids_file = await readFile('title_ids.json');
-    const ids = JSON.parse(title_ids_file.toString('utf8'));
-
-    ids.forEach((title) => {
-      titleIdToTitleMap[title.titleid] = title.title;
-    });
-  }
+  ) {}
 
   @Get('/')
   @Header('content-type', 'text/html')
-  async root() {
-    const sessions = await this.commandBus.execute(
+  async index() {
+    const file = await readFile('index.html');
+    return file.toString('utf8');
+  }
+
+  @Get('/sessions')
+  @Header('content-type', 'application/json')
+  async sessionData() {
+    const sessionsJSON = await this.commandBus.execute(
       new AggregateSessionCommand(),
     );
 
-    let rows = '';
-
-    sessions.forEach(async (session: Session) => {
-      const title =
-        titleIdToTitleMap[session.titleId.toString()] + ' - ' + session.titleId;
-      const row = `<tr><td> ${title} </td><td> ${session.players.length} </td></tr>`;
-
-      rows += row;
-    });
-
-    const file = await readFile('index.html');
-
-    let index = file.toString('utf8');
-    index = index.replace('{rows}', rows);
-
-    return index;
+    return sessionsJSON;
   }
 }
