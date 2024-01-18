04eae9b195bcd330961cafe84bb4b5a0ae002ee4
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2024-01-18 17:36:21 +0000

    Fixed failing HTTP requests

--- src/application/commandHandlers/AggregateSessionCommandHandler.ts
+++ src/application/commandHandlers/AggregateSessionCommandHandler.ts
@@ -4,7 +4,7 @@ import { AggregateSessionCommand } from '../commands/AggregateSessionCommand';
 import ISessionRepository, {
   ISessionRepositorySymbol,
 } from 'src/domain/repositories/ISessionRepository';
-import Session from 'src/domain/aggregates/Session';
+import axios, { AxiosRequestConfig } from 'axios';
 
 @CommandHandler(AggregateSessionCommand)
 export class AggregateSessionCommandHandler
@@ -15,6 +15,44 @@ export class AggregateSessionCommandHandler
     private repository: ISessionRepository,
   ) {}
 
+  async downloadImageAsBase64(url: string): Promise<string> {
+    const response = await this.downloadContent(url, 'arraybuffer');
+
+    if (response) {
+      const base64 = Buffer.from(response, 'binary').toString('base64');
+      return `data:image/png;base64,${base64}`;
+    }
+
+    return '';
+  }
+
+  async downloadContent(url: string, type: any): Promise<any> {
+    let data = undefined;
+
+    const config: AxiosRequestConfig = {
+      url: url,
+      responseType: type,
+    };
+
+    await axios
+      .get(url, config)
+      .then((response) => {
+        data = response.data;
+        console.log('Request Success');
+      })
+      .catch(function (error) {
+        if (error.response) {
+          console.log(`Failed ${url}`);
+        } else if (error.request) {
+          console.log(`Failed ${url}`);
+        } else {
+          console.log(`Failed ${url}`);
+        }
+      });
+
+    return data;
+  }
+
   async execute() {
     const sessions = await this.repository.findAllAdvertisedSessions();
 
@@ -22,17 +60,29 @@ export class AggregateSessionCommandHandler
 
     titles['Titles'] = [];
 
-    sessions.forEach((session: Session) => {
+    for (const session of sessions) {
       const titleId = session.titleId.toString();
 
       let index = titles['Titles'].findIndex(
         (title) => title.titleId === titleId,
       );
 
+      // will return object if title not found.
+      const icon_base64 = await this.downloadImageAsBase64(
+        `http://xboxunity.net/Resources/Lib/Icon.php?tid=${titleId}`,
+      );
+
+      const title_info = await this.downloadContent(
+        `http://xboxunity.net/Resources/Lib/Title.php?tid=${titleId}`,
+        'json',
+      );
+
       if (index == -1) {
         const data = {
           titleId: titleId,
           name: session.title,
+          icon: icon_base64,
+          info: title_info,
           sessions: [],
         };
 
@@ -48,7 +98,7 @@ export class AggregateSessionCommandHandler
       };
 
       titles['Titles'][index]['sessions'].push(data);
-    });
+    }
 
     return JSON.stringify(titles);
   }
