d188a0ee265aedf1aa772e241fc86accaf4fa269
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2024-06-19 16:17:17 -0400

    fixed compile issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
