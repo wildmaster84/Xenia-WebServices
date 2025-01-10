ce70b88573ad2e8ba7eabcf25d3f20b633466dad
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2025-01-10 18:15:57 -0500

    Updated Autoinstaller
    Co-Authored-By: AleNaz <154675640+a1enaz@users.noreply.github.com>

--- setup.bat
+++ setup.bat
@@ -1,22 +1,47 @@
 @echo off
-echo Node.js version: & node --version
-echo:
-echo npm version: & call npm --version
+setlocal enabledelayedexpansion
 
-echo:
-pause
-
+:MENU
 cls
+echo ==========================
+echo Choose an option:
+echo ==========================
+echo 1. Setup Server
+echo 2. Start Server
+echo 3. Exit
+==========================
+set /p choice=Enter your choice (1/2/3): 
 
+if "%choice%"=="1" goto SETUP_SERVER
+if "%choice%"=="2" goto START_SERVER
+if "%choice%"=="3" exit
+goto MENU
+
+:SETUP_SERVER
+cls
+echo ==========================
+echo Setup Server
+echo ==========================
+echo Node.js version: & node --version
+echo.
+echo npm version: & call npm --version
+echo.
+pause
+cls
 echo Installing...
 call npm install
-
-echo:
-
+echo.
 echo Building...
 call npm run build
-
-echo:
-
+echo.
 echo Starting...
-call npm run start:dev
+pause
+goto MENU
+
+:START_SERVER
+cls
+echo ==========================
+echo Starting Server...
+echo ==========================
+start cmd /k "npm run start"
+exit
