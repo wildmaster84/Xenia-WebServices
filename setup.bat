43644987ab92cdc98d412c6e1d222403babee96c
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2025-01-12 15:54:04 -0500

    Updated Setup Script (#43)
    Co-authored-by: AleNaz <154675640+a1enaz@users.noreply.github.com>

--- setup.bat
+++ setup.bat
@@ -1,22 +1,59 @@
 @echo off
-echo Node.js version: & node --version
-echo:
-echo npm version: & call npm --version
 
-echo:
-pause
-
+:MENU
 cls
+echo Choose an option:
+echo ==========================
+echo 1. Setup Server
+echo 2. Start Server
+echo 3. Exit
+set /p choice=Enter your choice: 
 
+if "%choice%"=="1" goto SETUP_SERVER
+if "%choice%"=="2" goto START_SERVER
+if "%choice%"=="3" exit
+goto MENU
+
+:SETUP_SERVER
+cls
+echo ==========================
+echo Setup Server
+echo ==========================
+echo Node.js version: & node --version
+echo.
+echo npm version: & call npm --version
+echo.
+pause
+cls
 echo Installing...
 call npm install
-
-echo:
-
+echo.
 echo Building...
 call npm run build
+echo.
 
-echo:
+:: Check if .env exists, just echo the result
+if exist "%~dp0\.env" (
+    echo .env already exists. Skipping renaming.
+) else (
+    echo .env does not exist. Renaming .env.template to .env...
+    if exist "%~dp0\.env.template" (
+        ren "%~dp0\.env.template" ".env"
+        echo .env.template renamed to .env successfully.
+    ) else (
+        echo .env.template not found. Please check the setup.
+    )
+)
 
-echo Starting...
-call npm run start:dev
+echo.
+echo Server setup completed.
+pause
+goto MENU
+
+:START_SERVER
+cls
+echo ==========================
+echo Starting Server...
+echo ==========================
+start cmd /k "npm run start"
+exit
